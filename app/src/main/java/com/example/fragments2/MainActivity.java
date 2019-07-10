c7a44a5289f944742c8fc44bb41266cf0721ec0b
janeDoe7 <50467708+janeDoe7@users.noreply.github.com>
2019-07-10 15:38:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/fragments2/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.fragments2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button firstFragment, secondFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // get the reference of Buttons
+        firstFragment = (Button) findViewById(R.id.Fragment1);
+        secondFragment = (Button) findViewById(R.id.Fragment2);
+
+        // perform setOnClickListener event on First Button
+        firstFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load First Fragment
+                loadFragment(new Fragment1());
+            }
+        });
+
+        // perform setOnClickListener event on Second Button
+        secondFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // load Second Fragment
+                loadFragment(new Fragment2());
+            }
+        });
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        // create a FragmentManager
+        FragmentManager fm = getFragmentManager();
+        // create a FragmentTransaction to begin the transaction and replace the Fragment
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        // replace the FrameLayout with new Fragment
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit(); // save the changes
+    }
+}
